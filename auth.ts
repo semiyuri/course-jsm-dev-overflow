9bfc4a8c911a111dfc162795f0b3844078211200
semiyuri <juriyunna27@gmail.com>
2024-11-14 02:29:09 +0200

    feat: implement authentication with google provider

--- auth.ts
+++ auth.ts
@@ -1,10 +1,12 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export enum AuthProvider {
   Github = "github",
+  Google = "google",
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub],
+  providers: [GitHub, Google],
 });
