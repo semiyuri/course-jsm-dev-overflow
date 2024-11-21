f8a30f1b4c77653becf1f4979379fe888ea76b88
semiyuri <juriyunna27@gmail.com>
2024-11-22 00:00:23 +0200

    feat: implement RightSidebar component and related settings

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/RightSidebar";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
@@ -13,6 +14,8 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );
