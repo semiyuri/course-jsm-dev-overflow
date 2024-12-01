5d878263dde9bca074564ad340819f870ec4b829
semiyuri <juriyunna27@gmail.com>
2024-12-01 21:04:41 +0200

    fix: adjust TagCard alignment and imports order

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -1,8 +1,10 @@
-import ROUTES from "@/constants/routes";
 import Link from "next/link";
 import React from "react";
-import { Badge } from "../ui/badge";
+
+import ROUTES from "@/constants/routes";
 import { getDeviconClassName } from "@/lib/utils";
+
+import { Badge } from "../ui/badge";
 
 interface Props {
   _id: string;
@@ -12,11 +14,14 @@ interface Props {
   compact?: boolean;
 }
 
-const TagCard = ({ _id, name, questions, showCount, compact }: Props) => {
+const TagCard = ({ _id, name, questions, showCount }: Props) => {
   const iconClass = getDeviconClassName(name);
 
   return (
-    <Link href={ROUTES.TAGS(_id)} className="flex justify-between gap-2">
+    <Link
+      href={ROUTES.TAGS(_id)}
+      className="flex items-center justify-between gap-2"
+    >
       <Badge className="subtle-medium background-light800_dark300 text-light400_light500 rounded-md border-none px-4 py-2 uppercase">
         <div className="flex-center space-x-2">
           <i className={`${iconClass} text-sm`}></i>
