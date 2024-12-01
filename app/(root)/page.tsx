1378e11a04ef1c511752da49c7cd6c05b8568dd2
semiyuri <juriyunna27@gmail.com>
2024-12-01 21:01:10 +0200

    feat: implement HomeFilter component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -9,10 +10,7 @@ const questions = [
     _id: "1",
     title: "How to learn React?",
     description: "I want to learn React, can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "JavaScript" },
-    ],
+    tags: [{ _id: "1", name: "React" }],
     author: { _id: "1", name: "John Doe" },
     upvotes: 10,
     answers: 5,
@@ -23,10 +21,7 @@ const questions = [
     _id: "2",
     title: "How to learn JavaScript?",
     description: "I want to learn JavaScript, can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "JavaScript" },
-    ],
+    tags: [{ _id: "2", name: "JavaScript" }],
     author: { _id: "1", name: "John Doe" },
     upvotes: 10,
     answers: 5,
@@ -54,11 +49,19 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query?.toLowerCase())
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query.toLowerCase());
+
+    const matchesFilter = filter
+      ? question.tags[0].name.toLowerCase() === filter.toLowerCase()
+      : true;
+
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
@@ -82,7 +85,7 @@ const Home = async ({ searchParams }: SearchParams) => {
         />
       </section>
 
-      <div>HomeFilter</div>
+      <HomeFilter />
 
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
